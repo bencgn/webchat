2fa7473c6a18e77af8674845bec1f674da195c4d
bencgn <trannguyenvy66@gmail.com>
2023-11-28 15:12:15 +0700

    upppp

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const fs = require('fs');
+const multer = require('multer');
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
@@ -12,43 +13,58 @@ const chatHistoryFile = 'chat_history.txt';
 
 // Function to append message to the file
 function saveMessage(message) {
-  fs.appendFileSync(chatHistoryFile, message + '\n', 'utf8');
+    fs.appendFileSync(chatHistoryFile, message + '\n', 'utf8');
 }
 
-// Load chat history and emit to newly connected client
 io.on('connection', (socket) => {
+    console.log('A user connected');
+    fs.readFile(chatHistoryFile, 'utf8', (err, data) => {
+        if (err) {
+            console.log('Error reading chat history:', err);
+            return;
+        }
+        socket.emit('chat history', data);
+    });
 
-  console.log('A user connected');
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+        saveMessage(msg);
+    });
 
-  fs.readFile(chatHistoryFile, 'utf8', (err, data) => {
-    if (err) {
-      console.log('Error reading chat history:', err);
-      return;
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+// Multer setup for file uploading
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'E:/webchat/file') // Upload directory
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now())
     }
-    socket.emit('chat history', data);
-  });
-
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-    saveMessage(msg);
-  });
-
-
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
-  });
-
-
-
 });
 
-server.listen(3000, '0.0.0.0', () => {
-  console.log('listening on *:3000');
+const upload = multer({ storage: storage });
+
+// Define the file upload route once
+app.post('/upload', upload.single('file'), (req, res) => {
+    const uploadedFileName = req.file.originalname;
+    const message = `User uploaded a file: ${uploadedFileName}`;
+
+    // Append this message to chat_history.txt
+    saveMessage(message);
+
+    // Emit an event to all connected clients
+    io.emit('file uploaded', {
+        filename: req.file.filename,
+        originalName: req.file.originalname
+    });
+
+    res.send('File uploaded successfully.');
 });
 
-
-
-
-
-
-
+server.listen(3001, '0.0.0.0', () => {
+    console.log('listening on *:3001');
+});
